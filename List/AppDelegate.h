0de3fd274fd28c6c0751daf7da2abe9d86e888a6
Richard Tyran <rictyran@Richards-MacBook-Air.local>
2015-01-07 09:47:48 -0500

    Initial Commit

---
+++ List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  List
+//
+//  Created by Richard Tyran on 1/7/15.
+//  Copyright (c) 2015 Richard Tyran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ List/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  List
+//
+//  Created by Richard Tyran on 1/7/15.
+//  Copyright (c) 2015 Richard Tyran. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
